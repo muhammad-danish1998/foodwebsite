76404574064a8739ac23a49673b8d04313c73eca
nadirdeveloper <nadirali.41006@gmail.com>
2023-01-30 12:23:39 +0500

    added navigation in signup and sign in

--- src/components/Header-Toggle.jsx
+++ src/components/Header-Toggle.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HeaderToggle = (props) => {
 //  const [toggleValue , setToggleValue] =  useState(0);
-
+const initalValue = window.location.pathname === "/signup" ? props.value2 : props.value1;
+  const [selectedValue, setSelectedValue] = useState(initalValue);
+  const navigate = useNavigate();
+  const handleToggleChange = () => {
+    if (selectedValue === props.value1) {
+      setSelectedValue(props.value2);
+      navigate("/signup");
+    } else {
+      navigate("/signin");
+      setSelectedValue(props.value1);
+    }
+  }
   return (
     <div>
       <label
         for="Toggle3"
         className="inline-flex items-center p-2 rounded-md cursor-pointer text-black  "
       >
-        <input id="Toggle3" type="checkbox" className="hidden peer "  />
+        <input checked={selectedValue === props.value2} onChange={() => handleToggleChange()} id="Toggle3" type="checkbox" className="hidden peer "  />
         <span className="px-4 py-2 flex items-center justify-center  mt-2 rounded-l-md text-lg bg-black h-8 text-white peer-checked:bg-gray-300">
       {props.value1}
         </span>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -20,7 +20,7 @@ export default function Signup() {
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Sing In or Sign Up
+          Sign In or Sign Up
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             <HeaderToggle value1={"Sign in"} value2="Sign Up" />
